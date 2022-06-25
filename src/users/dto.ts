bf5d78b89847b6a12338852fa75b6e7ac1284239
Amir <amir.malek.esfandiari@gmail.com>
2022-06-25 08:23:50 +0430

    add user controller  method

---
+++ src/users/dto.ts
@@ -0,0 +1,17 @@
+export class CreateUserDto {
+    name: string;
+    familyName : string ;
+    age : number ;
+    email: string ;
+    address: string ;
+    tell: string ;
+
+}
+
+
+export class ListAllEntities {
+    name: string ;
+    limit: number ;
+}
+
+
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, All, Put, Req, Res, Delete, Post, Body, Query, Param,} from '@nestjs/common';
+import {CreateUserDto, ListAllEntities} from "./dto";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return 'this action ads new user' ;
+    }
+
+    @Get()
+    findAll(@Query() query: ListAllEntities) {
+        return `this action return all cats (limit: ${query.limit} )items` ;
+    }
+
+    @Get(':id')
+    fineOne(@Param('id') id: string) {
+        return `this action return a #${id} cat` ;
+    }
+
+
+}
